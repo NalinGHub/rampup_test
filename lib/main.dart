da02cbf30b5bf9a3eec71c2ecdb38ecca58861e9
NalinGHub <nalinm01@gmail.com>
2022-02-18 16:14:00 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:workshop_test/screens/home.dart';
+import 'package:workshop_test/screens/login.dart';
+import 'package:workshop_test/services/auth.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return const Scaffold(
+              body: Center(
+                child: Text("Error"),
+              ),
+            );
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Root();
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return const Scaffold(
+            body: Center(
+              child: Text("Loading..."),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth(auth: _auth).user,
+      builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.data?.uid == null) {
+            return Login(
+              auth: _auth,
+              firestore: _firestore,
+            );
+          } else {
+            return Home(
+              auth: _auth,
+              firestore: _firestore,
+            );
+          }
+        } else {
+          return const Scaffold(
+            body: Center(
+              child: Text("Loading..."),
+            ),
+          );
+        }
+      }, //Auth stream
+    );
+  }
+}
+
